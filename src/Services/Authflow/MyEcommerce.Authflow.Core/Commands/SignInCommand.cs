0295f828f6816809cc67eae26d4e366fce11e0a6
Maks.Szokalski <42069493+illunix@users.noreply.github.com>
2022-10-03 01:39:42 +0200

    update

--- src/Services/Authflow/MyEcommerce.Authflow.Core/Commands/SignInCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MyEcommerce.Shared.Abstractions.Request;
-
-namespace MyEcommerce.Users.Application.Commands;
-
-internal readonly record struct SignInCommand(
-    string Email,
-    string Password
-) : IHttpRequest;
